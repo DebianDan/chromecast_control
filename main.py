f0bd06d58b7da78964d2a0dbbcb332d7ec44fb46
Dan <dantucholski@gmail.com>
2017-12-16 21:59:54 -0800

    add logging

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import pychromecast
+import logging
 
 CHROMECAST_NAME = 'Living Room'
 cast = None
 chromecast_ip = None
+
+logging.basicConfig(level=logging.DEBUG,
+	format='%(asctime)s %(levelname)s %(message)s',
+	filename='chromecast_control.log')
 
 try:
 	with open('.living_room_chromecast_ip', 'r') as f:
@@ -12,36 +17,38 @@ except Exception as e:
 	pass
 
 if chromecast_ip:
-	print "DEBUG: Retrieved saved Living Room Chromecast IP of: " + str(chromecast_ip)
+	logging.debug("Retrieved saved Living Room Chromecast IP of: " + str(chromecast_ip))
 	try:
 		cast = pychromecast.Chromecast(chromecast_ip)
 	except Exception as e:
-		print "WARN: Failed to connect directly: " + str(e)
+		logging.warning("Failed to connect directly: " + str(e))
 		pass
 	else:
 		cast.wait()
 
 if not cast:
-	print "INFO: Attempting to discover all chromecasts, fallback for direct connect"
+	logging.info("Attempting to discover all chromecasts, fallback for direct connect")
 	chromecasts = pychromecast.get_chromecasts()
 	if chromecasts:
-		cast = next(cc for cc in chromecasts if cc.device.friendly_name == CHROMECAST_NAME)
+		cast = next(cc for cc in chromecasts if cc.device.friendly_name == chromecast_name)
 		cast.wait()
 
 if cast and cast.device:
 	with open('.living_room_chromecast_ip', 'w') as f:
 		f.write(cast.host)
-		print "DEBUG: Stored Living Room Chromecast IP of: " + str(cast.host)
+		logging.debug("Stored Living Room Chromecast IP of: " + str(cast.host))
 	mc = cast.media_controller
 	mc.block_until_active()
 	if mc.status.player_is_playing:
 		mc.pause()
-		print "INFO: Paused " + CHROMECAST_NAME
+		logging.info("Paused " + chromecast_name)
 	elif mc.status.player_is_paused:
 		if mc.status.supports_seek:
 			mc.seek(max(0, mc.status.current_time - 10))
-			print "INFO: Rewinded 10 secs " + CHROMECAST_NAME
+			logging.info("Rewinded 10 secs " + chromecast_name)
 		mc.play()	
-		print "INFO: Played " + CHROMECAST_NAME
+		logging.info("Played " + chromecast_name)
 else:
-	print "ERROR: Could not establish connection with Living Room Chromecast.."
+	logging.error("Could not establish connection with Living Room Chromecast..")
+
+
