b7ba60a71f0a8bd6d79b46a02b571b80d88ea3be
Dan <dantucholski@gmail.com>
2017-12-16 23:12:06 -0800

    added RPi.GPIO and interrupt code

--- main.py
+++ main.py
@@ -1,54 +1,70 @@
+import RPi.GPIO as GPIO
 import pychromecast
 import logging
 
-CHROMECAST_NAME = 'Living Room'
-cast = None
-chromecast_ip = None
-
-logging.basicConfig(level=logging.DEBUG,
-	format='%(asctime)s %(levelname)s %(message)s',
-	filename='chromecast_control.log')
-
-try:
-	with open('.living_room_chromecast_ip', 'r') as f:
-		chromecast_ip = f.read()
-except Exception as e:
-	print e
-	pass
-
-if chromecast_ip:
-	logging.debug("Retrieved saved Living Room Chromecast IP of: " + str(chromecast_ip))
+def main_control():	
+	
+	CHROMECAST_NAME = 'kbTV'
+	cast = None
+	chromecast_ip = None
+	
 	try:
-		cast = pychromecast.Chromecast(chromecast_ip)
+		with open('.living_room_chromecast_ip', 'r') as f:
+			chromecast_ip = f.read()
 	except Exception as e:
-		logging.warning("Failed to connect directly: " + str(e))
+		logging.warning(str(e))
 		pass
+	
+	if chromecast_ip:
+		logging.info("Retrieved saved Living Room Chromecast IP of: " + str(chromecast_ip))
+		try:
+			cast = pychromecast.Chromecast(chromecast_ip)
+		except Exception as e:
+			logging.warning("Failed to connect directly: " + str(e))
+			pass
+		else:
+			cast.wait()
+	
+	if not cast:
+		logging.info("Attempting to discover all chromecasts, fallback for direct connect")
+		chromecasts = pychromecast.get_chromecasts()
+		if chromecasts:
+			cast = next(cc for cc in chromecasts if cc.device.friendly_name == CHROMECAST_NAME)
+			cast.wait()
+	
+	if cast and cast.device:
+		with open('.living_room_chromecast_ip', 'w') as f:
+			f.write(cast.host)
+			logging.info("Stored Living Room Chromecast IP of: " + str(cast.host))
+		mc = cast.media_controller
+		mc.block_until_active()
+		if mc.status.player_is_playing:
+			if mc.status.supports_seek:
+				mc.seek(max(0, mc.status.current_time - 10))
+				logging.info("Rewinded 10 secs " + CHROMECAST_NAME)
+			mc.pause()
+			logging.info("Paused " + CHROMECAST_NAME)
+		elif mc.status.player_is_paused:
+			mc.play()	
+			logging.info("Played " + CHROMECAST_NAME)
 	else:
-		cast.wait()
+		logging.error("Could not establish connection with Living Room Chromecast..")
 
-if not cast:
-	logging.info("Attempting to discover all chromecasts, fallback for direct connect")
-	chromecasts = pychromecast.get_chromecasts()
-	if chromecasts:
-		cast = next(cc for cc in chromecasts if cc.device.friendly_name == chromecast_name)
-		cast.wait()
+def main():
+	logging.basicConfig(level=logging.INFO,
+		format='%(asctime)s %(levelname)s %(message)s',
+		filename='chromecast_control.log')
+	
+	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+	
+	while True:
+		try:
+			GPIO.wait_for_edge(17, GPIO.FALLING)
+			main_control()
+		except:
+			break
+	GPIO.cleanup()
 
-if cast and cast.device:
-	with open('.living_room_chromecast_ip', 'w') as f:
-		f.write(cast.host)
-		logging.debug("Stored Living Room Chromecast IP of: " + str(cast.host))
-	mc = cast.media_controller
-	mc.block_until_active()
-	if mc.status.player_is_playing:
-		mc.pause()
-		logging.info("Paused " + chromecast_name)
-	elif mc.status.player_is_paused:
-		if mc.status.supports_seek:
-			mc.seek(max(0, mc.status.current_time - 10))
-			logging.info("Rewinded 10 secs " + chromecast_name)
-		mc.play()	
-		logging.info("Played " + chromecast_name)
-else:
-	logging.error("Could not establish connection with Living Room Chromecast..")
-
-
+if __name__ == '__main__':
+	main()
