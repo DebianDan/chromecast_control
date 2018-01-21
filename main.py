c1f493c1d569f8c6a6f771195f0adcb305175188
Dan <dantucholski@gmail.com>
2018-01-21 15:26:55 -0800

    Added LED support. 3v now, TODO add transistor/relay and run off 5v

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import time
 def main_control(pause_delay=0):	
 
 	start_time = time.time()	
+	#IDLE_APP_ID = 'E8C28D3C'
 	REWIND_PADDING = 10
 	CHROMECAST_NAME = 'Living Room'
 	cast = None
@@ -42,6 +43,8 @@ def main_control(pause_delay=0):
 		mc = cast.media_controller
 		mc.block_until_active()
 		casted_app = cast.status.display_name.lower()
+		#check if something is playing
+		#if cast.status is None or cast.app_id in (None, IDLE_APP_ID):
 		if mc.status.player_is_playing:
 			mc.pause()
 			pause_delay = int(time.time() - start_time)
@@ -60,27 +63,41 @@ def main_control(pause_delay=0):
 				mc.play()	
 				logging.info("Played " + CHROMECAST_NAME)
 		#mc.tear_down()
-		#cast.disconnect()
+		cast.disconnect()
 	else:
 		logging.error("Could not establish connection with Living Room Chromecast..")
+		blink_for_error()
+
+def blink_for_error():
+	for _ in range(4):
+		GPIO.output(22, GPIO.LOW)
+		time.sleep(0.3)
+		GPIO.output(22, GPIO.HIGH)
+		time.sleep(0.3)
 
 def main():
 	logging.basicConfig(level=logging.INFO,
 		format='%(asctime)s %(levelname)s %(message)s',
 		filename='/home/dan/chromecast_control/chromecast_control.log')
 	
+	GPIO.setwarnings(False)
 	GPIO.setmode(GPIO.BCM)
+	GPIO.setup(22, GPIO.OUT, initial=GPIO.LOW)
 	GPIO.setup(17, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 	pause_delay = 0
 	while True:
 		try:
 			GPIO.wait_for_edge(17, GPIO.FALLING)
+			GPIO.output(22, GPIO.HIGH)
 			pause_delay = main_control(pause_delay)
 		except KeyboardInterrupt:				
 			logging.info("KeyboardInterrupt ended program...")
 			break
 		except Exception as e:
 			logging.error("Encountered exception in main while loop: " + str(e))
+			blink_for_error()
+		finally:
+			GPIO.output(22, GPIO.LOW)
 	GPIO.cleanup()
 
 if __name__ == '__main__':
